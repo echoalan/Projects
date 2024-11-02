ae726d0c8e565e5910e2e74decfbf75ee8149af7
Alan Rojas <109986279+echoalan@users.noreply.github.com>
2024-11-02 01:13:41 -0300

    Update config.php

--- Turnero Hospital/login/model/config.php
+++ Turnero Hospital/login/model/config.php
@@ -1,9 +1,9 @@
 <?php
 
-    $servername = "98.142.101.90";
-    $username = "managerf_turno";
-    $password = "talonbrandzoea1";
-    $dbname = "managerf_turnos";
+    $servername = "";
+    $username = "";
+    $password = "";
+    $dbname = "";
    
     $conn = new mysqli($servername, $username, $password, $dbname);
     if ($conn->connect_error) {
